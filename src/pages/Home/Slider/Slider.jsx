2e9b70b2f565fb1396ec449161a52d7531d80d17
himaloyash <himaloyashis98@gmail.com>
2023-06-08 14:06:21 +0600

    carousel almost done

--- src/pages/Home/Slider/Slider.jsx
+++ src/pages/Home/Slider/Slider.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import './Slider.css'
 
 const Slider = () => {
     return (
-        <Carousel>
-            <div>
-                <img src="assets/1.jpeg" />
-                <p className="legend">Legend 1</p>
+        <Carousel className='text-[#B7B7B7]'>
+
+            <div className='img1'>
+                <p className=' text-3xl pt-8'>Learn Piano efficiently, that will take you to your goal.</p>
+                <br />
+                <p className='text-3xl '> Don't miss enrol in our Class.</p>
             </div>
-            <div>
-                <img src="assets/2.jpeg" />
-                <p className="legend">Legend 2</p>
+
+            <div className='img2'>
+                <p className=' text-3xl pt-8'>Learn Piano efficiently, that will take you to your goal.</p>
+                <br />
+                <p className='text-3xl '> Don't miss enrol in our Class.</p>
+
             </div>
-            <div>
-                <img src="assets/3.jpeg" />
-                <p className="legend">Legend 3</p>
+
+            <div className='img3'>
+                <p className=' text-3xl pt-8'>Learn Piano efficiently, that will take you to your goal.</p>
+                <br />
+                <p className='text-3xl '> Don't miss enrol in our Class.</p>
+            </div>
+
+            <div className='img4'>
+                <p className=' text-3xl pt-8'>Learn Piano efficiently, that will take you to your goal.</p>
+                <br />
+                <p className='text-3xl '> Don't miss enrol in our Class.</p>
             </div>
         </Carousel>
     );
