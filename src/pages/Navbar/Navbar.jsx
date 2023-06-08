403464e32c659ea1b9a0e3b6613443cc16658f01
himaloyash <himaloyashis98@gmail.com>
2023-06-08 11:58:53 +0600

    navbar added

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { FaUserCircle } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='bg-[#EEE2DE]  p-5 flex justify-between items-center'>
+            <div>
+                <h1>Musica Academy</h1>
+            </div>
+            <div className='flex gap-3'>
+                <p>Home</p>
+                <p>Instructors</p>
+                <p>Dashboard</p>
+            </div>
+            <div>
+                <FaUserCircle/>
+            </div>
         </div>
     );
 };
