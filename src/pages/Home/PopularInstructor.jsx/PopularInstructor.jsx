98e9d1f584762d238a2875ae33a2b22cd325866d
himaloyash <himaloyashis98@gmail.com>
2023-06-13 23:08:56 +0600

    aos animation added

--- src/pages/Home/PopularInstructor.jsx/PopularInstructor.jsx
+++ src/pages/Home/PopularInstructor.jsx/PopularInstructor.jsx
@@ -18,7 +18,7 @@ const PopularInstructor = () => {
     console.log(user)
     return (
         <div className='py-14'>
-            <h1 className='font-extrabold text-4xl text-center text-[#]'>Here are all famous instructor in Musica</h1>
+            <h1 className='font-extrabold text-4xl text-center pb-5 text-[#]'>Here are all famous instructor in Musica</h1>
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 py-6'>
                 {
                     user.map(pop => <PopularInstructorCard key={pop._id} pop={pop}></PopularInstructorCard>)
--- src/pages/Home/Slider/Slider.jsx
+++ src/pages/Home/Slider/Slider.jsx
@@ -7,26 +7,34 @@ const Slider = () => {
     return (
         <Carousel className='text-[#B7B7B7]'>
 
-            <div className='img1'>
-                <p className=' text-3xl pt-8 font-bold'>Learn Piano efficiently, to make your goal come true.</p>
+            <div className='img1' data-aos="fade-up"
+                data-aos-duration="3000">
+                <p className=' text-3xl pt-8 font-bold' data-aos="fade-up"
+                    data-aos-duration="3000">Learn Piano efficiently, to make your goal come true.</p>
                 <br />
-                <p className='text-3xl font-bold'> Don't miss, Enrol in our Class.</p>
+                <p className='text-3xl font-bold' data-aos="fade-up"
+                    data-aos-duration="3000"> Don't miss, Enrol in our Class.</p>
             </div>
 
-            <div className='img2'>
-                <p className=' text-3xl pt-8 font-bold'>Learn DJ efficiently, to make your goal come true.</p>
+            <div className='img2' data-aos="fade-up"
+                data-aos-duration="3000">
+                <p className=' text-3xl pt-8 font-bold' data-aos="fade-up"
+                    data-aos-duration="3000">Learn DJ efficiently, to make your goal come true.</p>
                 <br />
-                <p className='text-3xl font-bold'> Don't miss, Enrol in our Class.</p>
+                <p className='text-3xl font-bold' data-aos="fade-up"
+                    data-aos-duration="3000"> Don't miss, Enrol in our Class.</p>
 
             </div>
 
-            <div className='img3'>
+            <div className='img3' data-aos="fade-up"
+                data-aos-duration="3000">
                 <p className=' text-3xl pt-8 font-bold'>Learn Guitar efficiently, to make your goal come true.</p>
                 <br />
                 <p className='text-3xl font-bold'> Don't miss, Enrol in our Class.</p>
             </div>
 
-            <div className='img4'>
+            <div className='img4' data-aos="fade-up"
+                data-aos-duration="3000">
                 <p className=' text-3xl pt-8 font-bold'>Learn Dram efficiently, to make your goal come true.</p>
                 <br />
                 <p className='text-3xl font-bold'> Don't miss, Enrol in our Class.</p>
