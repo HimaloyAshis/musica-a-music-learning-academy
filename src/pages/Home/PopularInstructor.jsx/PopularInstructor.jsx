bfa35b22999bd1c7367d5d0d0c36723907b04cf2
himaloyash <himaloyashis98@gmail.com>
2023-06-27 23:47:56 +0600

    color set done

--- src/pages/Home/PopularInstructor.jsx/PopularInstructor.jsx
+++ src/pages/Home/PopularInstructor.jsx/PopularInstructor.jsx
@@ -17,8 +17,8 @@ const PopularInstructor = () => {
 
     console.log(user)
     return (
-        <div className='py-14'>
-            <h1 className='font-extrabold text-4xl text-center pb-5 text-[#]'>Here are all famous instructor in Musica</h1>
+        <div className='py-12'>
+            <h1 className='font-extrabold text-4xl text-center pb-5 text-[#2E8A99]'>Here are all famous instructor in Musica</h1>
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 py-6'>
                 {
                     user.map(pop => <PopularInstructorCard key={pop._id} pop={pop}></PopularInstructorCard>)
