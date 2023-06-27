e4f3b8342ad222577a7c91634fd74b03b35cf979
himaloyash <himaloyashis98@gmail.com>
2023-06-27 12:13:22 +0600

    useQuery ok

--- src/pages/Dashboard/CreatedClass/CreatedClass.jsx
+++ src/pages/Dashboard/CreatedClass/CreatedClass.jsx
@@ -14,6 +14,7 @@ const CreatedClass = () => {
 
     const {data: AllClass = []} = useQuery({
         queryKey: ['AllClass', user?.email],
+        enabled: !loading,
         queryFn: async ()=>{
             const res = await axiosSecure.get(`https://music-instrument-learning-server-himlaoy.vercel.app/class/instructor/${user?.email}`)
             return res.dada
