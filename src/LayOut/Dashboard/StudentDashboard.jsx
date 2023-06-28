918ad876d09b546f55b4e0ff42c66e0a052ca160
himaloyash <himaloyashis98@gmail.com>
2023-06-28 12:31:53 +0600

    all done

--- src/LayOut/Dashboard/StudentDashboard.jsx
+++ src/LayOut/Dashboard/StudentDashboard.jsx
@@ -8,7 +8,7 @@ const StudentDashboard = () => {
             <li><Link to={'/'}><FaHome></FaHome> Student Home</Link></li>
             <li><Link to={'/dashboard/selectedClass'}><FaPlus></FaPlus> My class</Link></li>
             <li><Link to={'/dashboard/paySuccessClass'}><FaThList></FaThList>Enrolled Class</Link></li>
-            <li><Link to={'/dashboard/payment'}><FaWallet></FaWallet> Payment</Link></li>
+            <li><Link to={'/dashboard/payment/hi99999965'}><FaWallet></FaWallet> Payment</Link></li>
             <li><Link to={'/dashboard/paymentHistory'}><FaHistory></FaHistory> Payment History</Link></li>
             <div className="divider">OR</div>
             <li><Link><FaHome></FaHome> User Home</Link></li>
