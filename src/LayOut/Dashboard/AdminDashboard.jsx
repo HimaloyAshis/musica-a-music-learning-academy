622149499eb78f972cd36d29861860a337bafc08
himaloyash <himaloyashis98@gmail.com>
2023-11-04 15:36:30 +0600

    all updated

--- src/LayOut/Dashboard/AdminDashboard.jsx
+++ src/LayOut/Dashboard/AdminDashboard.jsx
@@ -5,13 +5,9 @@ import { Link } from 'react-router-dom';
 const AdminDashboard = () => {
     return (
         <div className='text-[#1c3a45]'>
-            <li><Link to={'/'}><FaHome></FaHome> Admin Home</Link></li>
-            <li><Link to={'/dashboard/allUser'}><FaStoreAlt></FaStoreAlt> Manage users</Link></li>
-            <li><Link to={'/dashboard/allClass'}><FaStoreAlt></FaStoreAlt> Manage class</Link></li>
-            <div className="divider">OR</div>
-            <li><Link><FaHome></FaHome> User Home</Link></li>
-            <li><Link><FaHome></FaHome> User Home</Link></li>
-            <li><Link><FaHome></FaHome> User Home</Link></li>
+            <li className='text-xl font-bold'><Link to={'/'}><FaHome></FaHome> Admin Home</Link></li>
+            <li className='text-xl font-bold'><Link to={'/dashboard/allUser'}><FaStoreAlt></FaStoreAlt> Manage users</Link></li>
+            <li className='text-xl font-bold'><Link to={'/dashboard/allClass'}><FaStoreAlt></FaStoreAlt> Manage class</Link></li>
         </div>
     );
 };
--- src/LayOut/Dashboard/StudentDashboard.jsx
+++ src/LayOut/Dashboard/StudentDashboard.jsx
@@ -7,15 +7,12 @@ const StudentDashboard = () => {
     const [AllClasses] = AllCreatedClass()
     return (
         <div className='text-[#1c3a45]'>
-            <li><Link to={'/'}><FaHome></FaHome> Student Home</Link></li>
-            <li><Link to={'/dashboard/selectedClass'}><FaPlus></FaPlus> My class</Link></li>
-            <li><Link to={'/dashboard/paySuccessClass'}><FaThList></FaThList>Enrolled Class</Link></li>
-            <li><Link to={`/dashboard/payment/${AllClasses._id}`}><FaWallet></FaWallet> Payment</Link></li>
-            <li><Link to={'/dashboard/paymentHistory'}><FaHistory></FaHistory> Payment History</Link></li>
-            <div className="divider">OR</div>
-            <li><Link><FaHome></FaHome> User Home</Link></li>
-            <li><Link><FaHome></FaHome> User Home</Link></li>
-            <li><Link><FaHome></FaHome> User Home</Link></li>
+            <li className='text-xl font-bold'><Link to={'/'}><FaHome></FaHome> Student Home</Link></li>
+            <li className='text-xl font-bold'><Link to={'/dashboard/selectedClass'}><FaPlus></FaPlus> My class</Link></li>
+            <li className='text-xl font-bold'><Link to={'/dashboard/paySuccessClass'}><FaThList></FaThList>Enrolled Class</Link></li>
+            {/*  <li><Link to={`/dashboard/payment/${AllClasses._id}`}><FaWallet></FaWallet> Payment</Link></li> */}
+            <li className='text-xl font-bold'><Link to={'/dashboard/paymentHistory'}><FaHistory></FaHistory> Payment History</Link></li>
+            
         </div>
     );
 };
