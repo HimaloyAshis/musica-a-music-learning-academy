6242e0969d4cfbeed578998809910ba789018d8b
himaloyash <himaloyashis98@gmail.com>
2023-11-04 13:05:13 +0600

    navbar updated

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import useAuth from '../../Hook/useAuth';
 import { toast } from 'react-toastify';
+import { XMarkIcon, Bars3Icon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
 
-    const { user,logOut } = useAuth()
+    const { user, logOut } = useAuth()
+    const [Open, setOpen] = useState(false)
 
 
 
@@ -17,26 +19,39 @@ const Navbar = () => {
     }
 
     return (
-        <div className='bg-[#EEE2DE]  p-5 gap-2  lg:flex lg:justify-between items-center'>
-            <div className='sm:my-2'>
-                <h1 className='text-xl font-bold  text-[#5C8984]'>Musica</h1>
+
+        <div>
+            <div className=''>
+                <span onClick={() => setOpen(!Open)} className='md:hidden'>
+
+                    {
+                        Open === true ? < XMarkIcon className="h-6 w-6 text-blue-500 cursor-wait" />
+                            : <Bars3Icon className="h-6 w-6 text-blue-500 cursor-pointer" />
+                    }
+                </span>
             </div>
-            <div className='lg:flex  my-2 gap-3'>
-                <Link className='butn '>Home</Link>
-                <Link to={'/instructors'} className='butn'>Instructors</Link>
-                <Link className='butn' to={'/class'}>Class</Link>
-                { user ? 
-                <><Link className='butn' to={'/dashboard/instructor'}>Dashboard</Link>
-                <Link className='butn' onClick={handleLogout}>Logout</Link>
-                <div className="w-24 sm:mt-2 rounded-full">
-                    <img className='w-10 h-8  rounded-full' src={user.photoURL} />
-                </div></>
-                :
-                <Link className='butn' to={'/login'}>Login</Link>}
+            <div className='bg-[#164863]  p-5 gap-2  lg:flex lg:justify-between items-center'>
+                <div className='sm:my-2'>
+                    <h1 className='text-2xl font-bold  text-[#bdcfcd]'>Musica</h1>
+                </div>
+                <div className='lg:flex  my-2 gap-3'>
+                    <Link className='butn '>Home</Link>
+                    <Link to={'/instructors'} className='butn'>Instructors</Link>
+                    <Link className='butn' to={'/class'}>Class</Link>
+                    {user ?
+                        <><Link className='butn' to={'/dashboard/instructor'}>Dashboard</Link>
+                            <Link className='butn' onClick={handleLogout}>Logout</Link>
+                            <div className="w-24 sm:mt-2 rounded-full">
+                                <img className='w-10 h-8  rounded-full' src={user.photoURL} />
+                            </div></>
+                        :
+                        <Link className='butn' to={'/login'}>Login</Link>}
+
+                </div>
 
             </div>
-
         </div>
+
     );
 };
 
